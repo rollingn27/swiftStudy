41736bc64b22d350598019397963456b06aaf2fb
rollingn27 <arollingstone27@gmail.com>
2024-03-17 13:22:06 +0900

    chapter 01

---
+++ MyMemory/MyMemory/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by minkuyoun on 3/16/24.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,8 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by minkuyoun on 3/16/24.
+//
+
+import Foundation
